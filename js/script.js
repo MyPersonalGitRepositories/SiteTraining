b5e9e027966b04b0cb3797343c7e0dc359b0bbc7
Maksym Pidlisnyi <p.maxsym@gmail.com>
2019-10-31 19:39:28 +0200

    Fix bugs

--- js/script.js
+++ js/script.js
@@ -175,14 +175,16 @@ function displayCart() {
     var cartArray = shoppingCart.listCart();
     var output = "";
     for (var i in cartArray) {
-        output += "<tr>"
+        output += "<br>"
             + "<td>" + cartArray[i].name + "</td>"
-            + "<td>(" + cartArray[i].price + ")</td>"
-            + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
-            + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
-            + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
-            + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
-            + " = "
+            + "<td>" + cartArray[i].price + " hrn.</td>"
+            + "<br>"
+            + "<td><button class='minus-item btn btn-primary' data-name=" + cartArray[i].name + "><span class='glyphicon glyphicon-minus'></span></button></td>"
+            // + "<td><input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'></td>"
+            + "<td><span >" + cartArray[i].count + "</span></td>"
+            + "<td><button class='plus-item btn btn-primary' data-name=" + cartArray[i].name + "><span class='glyphicon glyphicon-plus'></span></button></td>"
+            + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + "><span class='glyphicon glyphicon-trash'></span></button></td>"
+            // + " = "
             + "<td>" + cartArray[i].total + "</td>"
             + "</tr>";
     }
